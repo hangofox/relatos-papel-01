5fa445b71ed11be17bf5c00bbca06809872a84e8
Name <email>
2025-12-22 18:59:17 -0500

    ACTUALIZACION DEL CSS, CREACION CARPETA DE IMAGENES Y CREACION DEL LOGO, NAVBAR.JSX Y EL HOME.JSX

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,43 @@ import "../css/estilos-sitio-web.css";
 
 export const Navbar = () => {
   return (
-    <table width="100%" border="1" align="center" cellPadding="0" cellSpacing="0" className="cabezote">
+    <table width="100%" border="0" align="center" cellPadding="0" cellSpacing="0" className="tabla-cabezote">
       <tbody>
         <tr valign="top">
-          <td width="10%">LOGO.</td>
-          <td width="18%">LIBRERÍA RELATOS DE PAPEL.</td>
-          <td width="18%">Inicio.</td>
-          <td width="18%">Búsqueda.</td>
-          <td width="18%">
-            Hola<br />Nombres y apellidos usuario.
+          <td width="10%">
+            <div class="logo">
+               &nbsp;
+            </div>
           </td>
-          <td width="18%">Carrito de compras.</td>
+          <td width="26%">
+            <div class="titulo-sitio-web">
+               LIBRERÍA RELATOS DE PAPEL.
+            </div>
+          </td>
+          <td width="18%">
+            <div class="opcion-menu-superior">
+               <a href="#" title="Inicio." target="_self"><u>Inicio.</u></a>
+            </div>
+          </td>
+          <td width="18%">
+            <div class="opcion-menu-superior">
+               <a href="#" title="Búsqueda." target="_self">Búsqueda.</a>
+            </div>
+          </td>
+          <td width="18%">
+            <div class="opcion-menu-superior">
+               Hola<br/><strong><a href="#" title="Perfíl." target="_self">Nombres y apellidos usuario.</a></strong>
+            </div>
+          </td>
+          <td width="10%">
+            <div class="opcion-menu-superior">
+               <a href="#" title="Carrito de Compras." target="_self">
+                 <div class="icono-carrito-compras">
+                    &nbsp;
+                 </div>
+               </a>
+            </div>
+          </td>
         </tr>
       </tbody>
     </table>
