8c64097818a36b7fdf53a92cb9ea0284e7023f4d
Name <email>
2025-12-22 19:29:31 -0500

    ACTUALIZACION DEL CSS, CREACION CARPETA DE IMAGENES Y CREACION DEL LOGO, NAVBAR.JSX Y EL HOME.JSX

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "../css/estilos-sitio-web.css";
+
+export const Footer = () => {
+  return (
+    <table width="100%" border="1" align="center" cellPadding="0" cellSpacing="0" className="tabla-footer">
+      <tbody>
+        <tr valign="top">
+          <td width="100%">
+            <div class="titulo-footer">
+               Grupo 11 - MISSI UNIR.<br>Todos los Derechos Reservados - Bogotá DC, @ Colombia / Quito, @ Ecuador 2026.
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
