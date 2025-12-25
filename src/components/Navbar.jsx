1c1bdb7e925da118d2c28807c3928fa8814d4ce9
Gabby Zapata <gabby.ecuador@gmail.com>
2025-12-25 14:19:57 -0500

    Implementa menú colapsado y decoración del ítem

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,56 @@
-import React, { PureComponent } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Navbar = () => {
+  //Estado para controlar el colapso del menú
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+  //Controlar qué menú está activo
+  const [activeMenu, setActiveMenu] = useState('INI');
+
+  //Handler para indicar el colapso del menú
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+  //Función que implementa las dos funciones para el menú
+  const handleMenu = (collapsed, idMenu) =>{
+    setActiveMenu(idMenu);
+    setIsNavCollapsed(collapsed);
+  }
+
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
         <img
           src="/img/logo.png"
           alt="Logo"
           width="50"
           className="d-inline-block align-text-top me-2"
         />
-        <a class="navbar-brand" href="#">Librería Relatos de Papel</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <Link className="navbar-brand" to="/">Librería Relatos de Papel</Link>
+        
+        {/* Botón Toggler: agregamos el evento onClick */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          aria-controls="navbarSupportedContent" 
+          aria-expanded={!isNavCollapsed ? true : false} 
+          aria-label="Toggle navigation"
+          onClick={handleNavCollapse}
+        >
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <Link className="nav-link active"  to="/">Inicio</Link>
+
+        {/* Contenedor colapsable: manejamos la clase 'show' condicionalmente */}
+        <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`} id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className={activeMenu === 'INI' ? "nav-link active" : "nav-link"} to="/" onClick={() => handleMenu(true, 'INI')}>Inicio</Link>
             </li>
-            <li class="nav-item">
-              <Link className="nav-link"  to="/search">Búsqueda</Link>
+            <li className="nav-item">
+              <Link className={activeMenu === 'BUS' ? "nav-link active" : "nav-link"} to="/search" onClick={() => handleMenu(true, 'BUS')}>Búsqueda</Link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <Link className="nav-link" href="#">
                 <img src="/img/shoppincart.png" width="25" alt="Shopping-cart" />
-              </a>
+              </Link>
             </li>
           </ul>
         </div>
