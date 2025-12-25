485f9fccfcfd5c9c4c061fa2efb4e737c786920b
Gabby Zapata <gabby.ecuador@gmail.com>
2025-12-25 18:31:09 -0500

    Se completa la page del libro

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Books } from '../data/Data';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export const BookPage = () => {
   const { id } = useParams();
@@ -24,6 +25,35 @@ export const BookPage = () => {
           <p className='mt-2'>
             { book.synopsis }
           </p>
+          <div className='row container-fluid'>
+            <div className='col-3 text-end'>
+            <label htmlFor="id_modalidad">Modalidad:</label>
+            </div>
+            <div className="col-3">
+              <select className='field form-select' name="id_modalidad" id="id_modalidad">
+                <option selected>Escoja</option>
+                <option value="F">Físico</option>
+                <option value="D">Digital</option>
+              </select>
+            </div>
+            <div className="col-3 text-end">
+              <label htmlFor="cantidad">Cantidad:</label>
+            </div>
+            <div className="col-3">
+              <input type="number" className='field form-control text-end' name="cantidad" id="cantidad" value="1" />
+            </div>
+          </div>
+          <div className='row container-fluid align-items-center mt-2'>
+            <div className="col-6 text-end fw-bold">
+              <label>${book.price}</label>
+            </div>
+            <div className="col-6">
+              <button type="button" className='button-blue'>
+                <i className="bi bi-cart-fill p-2"></i>
+                Añadir al carrito
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
